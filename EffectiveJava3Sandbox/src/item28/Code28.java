04a27b09699baea0929b7263974435e9f04facdd
yuyabu <yuyabushita@gmail.com>
2018-01-03 07:21:32 +0900

    genericsと配列で失敗するコードを書いた
    型のミスマッチによるエラーはgenericsならコンパイル時にわかるが、配列なら実行時にしかわからない

--- EffectiveJava3Sandbox/src/item28/Code28.java
+++ EffectiveJava3Sandbox/src/item28/Code28.java
@@ -1,5 +1,8 @@
 package item28;
-
+/**
+ * Item 28:Prefer lists to arrays
+ */
 public class Code28 {
+	
 
 }
--- EffectiveJava3Sandbox/src/item28/Test28.java
+++ EffectiveJava3Sandbox/src/item28/Test28.java
@@ -2,11 +2,27 @@ package item28;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * Item28:Prefer lists to arrays
+ */
 class Test28 {
 
 	@org.junit.jupiter.api.Test
 	void test() {
-		fail("Not yet implemented");
+		
+		//配列とgenericsの一部を検証するコード
+
+		//失敗するコード(配列)
+		//Object[] objectArray = new Long[1];
+		//objectArray[0] = "I dont fit in";//実行時例外
+		
+		//失敗するコード(generics)
+		//List<Object> ol = new ArrayList<Long>();
+		//Type mismatch: cannot convert from ArrayList<Long> to List<Object>
+		//ol.add("I dont fit in");
+		
 	}
 
 }
