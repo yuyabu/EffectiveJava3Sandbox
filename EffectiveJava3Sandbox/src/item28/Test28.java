b571f1d3d938b81365e21853c31f9f56d1bf6c57
yuyabu <yuyabushita@gmail.com>
2018-01-03 08:01:28 +0900

    エラーメッセージの追記

--- EffectiveJava3Sandbox/src/item28/Test28.java
+++ EffectiveJava3Sandbox/src/item28/Test28.java
@@ -17,6 +17,7 @@ class Test28 {
 		//失敗するコード(配列)
 		//Object[] objectArray = new Long[1];
 		//objectArray[0] = "I dont fit in";//実行時例外
+		//ArrayStoreException
 		
 		//失敗するコード(generics)
 		//List<Object> ol = new ArrayList<Long>();
