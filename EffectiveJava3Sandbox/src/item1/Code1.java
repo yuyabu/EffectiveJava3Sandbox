94119fa94a1a3964fe58ae5e82a9892662867251
yuyabu <yuyabushita@gmail.com>
2018-01-08 22:23:51 +0900

    EnumSetの動作確認を追記

---
+++ EffectiveJava3Sandbox/src/item1/Code1.java
@@ -0,0 +1,12 @@
+package item1;
+
+public class Code1 {
+	public Code1(){}
+	
+	public enum SampleEnum{
+		No1,No2
+	}
+	public enum tooBigEnum{
+		No1,No2,No3,No4,No5,No6,No7,No8,No9,No10,No11,No12,No13,No14,No15,No16,No17,No18,No19,No20,No21,No22,No23,No24,No25,No26,No27,No28,No29,No30,No31,No32,No33,No34,No35,No36,No37,No38,No39,No40,No41,No42,No43,No44,No45,No46,No47,No48,No49,No50,No51,No52,No53,No54,No55,No56,No57,No58,No59,No60,No61,No62,No63,No64,No65
+	}
+}
--- EffectiveJava3Sandbox/src/item1/Test1.java
+++ EffectiveJava3Sandbox/src/item1/Test1.java
@@ -3,9 +3,14 @@ package item1;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.math.BigInteger;
+import java.util.EnumSet;
 import java.util.Random;
+import java.util.Set;
 
 import org.junit.jupiter.api.Test;
+
+import item1.Code1.SampleEnum;
+import item1.Code1.tooBigEnum;
 
 class Test1 {
 
@@ -25,6 +30,23 @@ class Test1 {
 		
 		Animal animal = Animal.newDog("potchi");
 		animal.bark();
+		
+		//EnumSetの動作確認
+		Set<SampleEnum> flg = EnumSet.noneOf(Code1.SampleEnum.class);
+
+		
+		Set<tooBigEnum> flg2 = EnumSet.noneOf(Code1.tooBigEnum.class);
+		Set<tooBigEnum> flg3 = EnumSet.noneOf(Code1.tooBigEnum.class);
+		Set<tooBigEnum> flg4 = EnumSet.noneOf(Code1.tooBigEnum.class);
+		Set<tooBigEnum> flg5 = EnumSet.noneOf(Code1.tooBigEnum.class);
+		
+		flg3.add(Code1.tooBigEnum.No1);//elements = 1
+		flg4.add(Code1.tooBigEnum.No2);// elements = 2
+		flg5.add((Code1.tooBigEnum.No3));//elements[0] = 4
+		flg5.add(Code1.tooBigEnum.No65);// elements[1]= 1
+		
+ 		System.out.println();
 	}
+	
 
 }
