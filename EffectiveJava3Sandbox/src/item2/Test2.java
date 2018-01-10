a28a86cf65fa6f4a1c434044d6d8e9f449bd43a4
yuyabu <yuyabushita@gmail.com>
2018-01-10 22:40:10 +0900

    JavaBeansパターンの実装をついき

---
+++ EffectiveJava3Sandbox/src/item2/Test2.java
@@ -0,0 +1,25 @@
+package item2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Test2 {
+
+	@Test
+	void test() {
+		
+		//テレスコーピングコンストラクタ
+		NutritionFacts1 cocaCola1 = new NutritionFacts1(240,8,100,0,35,27);
+		
+		//JavaBeansパターン
+		NutritionFacts2 cocaCola2 = new NutritionFacts2();
+		cocaCola2.setServingSize(240);
+		cocaCola2.setCalories(100);
+		cocaCola2.setSodium(35);
+		cocaCola2.setCarbohydrate(27);
+		
+		
+	}
+
+}
