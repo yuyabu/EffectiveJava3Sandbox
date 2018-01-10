2e6d27d9661f9a7d1cb92a55f647f2288f73c302
yuyabu <yuyabushita@gmail.com>
2018-01-10 23:51:50 +0900

    可読性の向上

--- EffectiveJava3Sandbox/src/item2/NutritionFacts.py
+++ EffectiveJava3Sandbox/src/item2/NutritionFacts.py
@@ -1,13 +1,24 @@
 class NutritionFacts:
-    def __init__(self,servingSize,servings,calories=0,fat=0,sodium=0,carbohydrate=0):
+    def __init__(self,
+                 servingSize,
+                 servings,
+                 calories=0,
+                 fat=0,
+                 sodium=0,
+                 carbohydrate=0):
         self.servingSize = servingSize;
         self.servings = servings;
         self.calories = calories;
         self.fat = fat;
         self.sodium = sodium;
         self.carbohydrate = carbohydrate;
-    # JavaでいうtoString()
+    # toString() in Java...
     def __str__(self):
-        return  " servingSize; = " + str(self.servingSize)  + ",\n"+ " servings; = " + str(self.servings)  + ",\n"+ " calories; = " + str(self.calories)  + ",\n"+ " fat; = " + str(self.fat)  + ",\n"+ " sodium; = " + str(self.sodium)  + ",\n"+ " carbohydrate; = " + str(self.carbohydrate)  + ",\n"
+        return  " servingSize; = " + str(self.servingSize) \
+      + ",\n"+ " servings; = " + str(self.servings) \
+      + ",\n"+ " calories; = " + str(self.calories)  \
+      + ",\n"+ " fat; = " + str(self.fat) \
+      + ",\n"+ " sodium; = " + str(self.sodium) \
+      + ",\n"+ " carbohydrate; = " + str(self.carbohydrate) + ",\n"
 ins = NutritionFacts(servingSize=240,servings=8,calories=100,sodium=35,carbohydrate=27)
 print(ins)
