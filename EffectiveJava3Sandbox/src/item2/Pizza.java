a4a951de356510514bfd544108fbf5df54e64029
yuyabu <yuyabushita@gmail.com>
2018-01-11 12:28:25 +0900

    pizzaクラスを実装

--- EffectiveJava3Sandbox/src/item2/Pizza.java
+++ EffectiveJava3Sandbox/src/item2/Pizza.java
@@ -1,5 +1,25 @@
 package item2;
 
-public class Pizza {
+import java.util.EnumSet;
+import java.util.Objects;
+import java.util.Set;
 
+public abstract class Pizza {
+	public enum Topping {HAM,MUSHROOM,ONION,PEPPER,SAUSAGE}
+	final Set<Topping> toppings;
+	abstract static class Builder<T extends Builder<T>>{
+		EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);
+		public T addToping(Topping topping) {
+			toppings.add(Objects.requireNonNull(topping));
+			return self();
+		}
+		abstract Pizza build();
+		
+		//サブクラスはthisを返せるよう必ずこのメソッドをオーバーライドすること
+		protected abstract T self();
+	}
+	Pizza(Builder<?> builder){
+		toppings = builder.toppings.clone();//item 50を参照
+	}
+	
 }
