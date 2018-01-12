f76c8c49866769fb64fa35153bc53f960cc56de1
yuyabu <yuyabushita@gmail.com>
2018-01-12 19:49:35 +0900

    private constructorの使用例を追記

---
+++ EffectiveJava3Sandbox/src/item4/Test4.java
@@ -0,0 +1,23 @@
+package item4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Test4 {
+
+	@Test
+	void test() {
+		UtilityClass.printf("hello world");
+		
+		//実行不可
+		//UtilityClass test = new UtilityClass();
+		//message:The constructor UtilityClass() is not visible
+		
+		//リフレクションによる攻撃を追記予定
+		
+		
+		
+	}
+
+}
