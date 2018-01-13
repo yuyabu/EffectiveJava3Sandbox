6d4f68c1a1ea9e920c911760242b412150033cf3
yuyabu <yuyabushita@gmail.com>
2018-01-13 23:18:45 +0900

    Singletonに対する攻撃例を追加

--- EffectiveJava3Sandbox/src/item3/Test3.java
+++ EffectiveJava3Sandbox/src/item3/Test3.java
@@ -3,19 +3,24 @@ package item3;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.lang.reflect.AccessibleObject;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 import org.junit.jupiter.api.Test;
 
 class Test3 {
 
 	@Test
-	void test() {
+	void test() throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		Elvis onlyOneElvis = Elvis.INSTANCE;
 		Paul onlyOnePaul = Paul.getInstance();
 		
-		//TODO:リフレクションを利用した上記２クラスへの攻撃を追記予定
-		
+		Constructor<Elvis> testConstructor = Elvis.class.getDeclaredConstructor();
+		testConstructor.setAccessible(true);
+		Elvis fakeElvis = testConstructor.newInstance();
 		//最強のシングルトン。
+		System.out.println(fakeElvis == onlyOneElvis);
 		DalisDaughter onlyOneDaughter =  DalisDaughter.INSTANCE;
 	}
 
