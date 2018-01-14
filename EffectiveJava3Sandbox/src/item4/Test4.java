24a59737cd892bb394e5752b594b82cf5e7be010
yuyabu <yuyabushita@gmail.com>
2018-01-14 19:37:43 +0900

    リフレクションの攻撃を追加。
    だが、try-catchできない。なんで？

--- EffectiveJava3Sandbox/src/item4/Test4.java
+++ EffectiveJava3Sandbox/src/item4/Test4.java
@@ -2,12 +2,17 @@ package item4;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 import org.junit.jupiter.api.Test;
+
+import item3.Elvis;
 
 class Test4 {
 
 	@Test
-	void test() {
+	void test() throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
 		UtilityClass.printf("hello world");
 		
 		//実行不可
@@ -15,7 +20,15 @@ class Test4 {
 		//message:The constructor UtilityClass() is not visible
 		
 		//リフレクションによる攻撃を追記予定
-		
+		try {
+			Constructor<UtilityClass> testConstructor = UtilityClass.class.getDeclaredConstructor();
+			testConstructor.setAccessible(true);
+			
+			UtilityClass fakeElvis = testConstructor.newInstance();
+		}catch(AssertionError e) {
+			System.out.println("コンストラクタの実行に失敗(エラーを受け取った　)");
+		}
+
 		
 		
 	}
