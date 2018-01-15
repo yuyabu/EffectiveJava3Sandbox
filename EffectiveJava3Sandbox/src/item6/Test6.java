f04e0dd8c9262ac603903971d1143fad30c0bf8a
yuyabu <yuyabushita@gmail.com>
2018-01-15 20:26:34 +0900

    非推奨のインスタンス化の例を作成

---
+++ EffectiveJava3Sandbox/src/item6/Test6.java
@@ -0,0 +1,14 @@
+package item6;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Test6 {
+
+	@Test
+	void test() {
+		String s = new String("bikini");
+	}
+
+}
