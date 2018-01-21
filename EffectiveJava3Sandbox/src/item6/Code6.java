b65b274b427b382a6bc696b9dc7df022b0d57b73
yuyabu <yuyabushita@gmail.com>
2018-01-22 00:41:02 +0900

    正規表現のpatternを再利用する方法を追記

--- EffectiveJava3Sandbox/src/item6/Code6.java
+++ EffectiveJava3Sandbox/src/item6/Code6.java
@@ -1,9 +1,17 @@
 package item6;
 
+import java.util.regex.Pattern;
+
 public class Code6 {
 	static boolean isRomanNumeral(String s) {
 		return s.matches("^(?=.)M*(C[MD]|D?C{0,3})"
 				+ "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
+	}
+	
+	private static final Pattern ROMAN = Pattern.compile("^(?=.)M*(C[MD]|D?C{0,3})"
+			+ "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
+	static boolean isRomanNumeral2(String s) {
+		return ROMAN.matcher(s).matches();
 	}
 
 }
