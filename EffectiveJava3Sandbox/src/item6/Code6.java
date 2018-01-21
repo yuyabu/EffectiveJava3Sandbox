ae04413a8a13c0ac82791d0640c51a11cae55415
yuyabu <yuyabushita@gmail.com>
2018-01-22 00:46:18 +0900

    遅延初期化(razy initialization)の説明を追記

--- EffectiveJava3Sandbox/src/item6/Code6.java
+++ EffectiveJava3Sandbox/src/item6/Code6.java
@@ -12,6 +12,10 @@ public class Code6 {
 			+ "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
 	static boolean isRomanNumeral2(String s) {
 		return ROMAN.matcher(s).matches();
+		//この書き方はパフォーマンス改善になるが、メソッドが呼ばれない場合にも
+		//Staticメンバのインスタンスが不用意に生成されてしまう。
+		//遅延初期化(azy initialization)で抑制することができるが
+		//遅延初期化は実装を複雑にする割に、あまりパフォーマンスを改善しないので非推奨。
 	}
 
 }
