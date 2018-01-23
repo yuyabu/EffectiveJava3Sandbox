bfd977e438e937c8c2ae6b9c0f9225f5950c56bb
yuyabu <yuyabushita@gmail.com>
2018-01-23 21:17:09 +0900

    row typeとObjectジェネリクスの違いを書いた

--- EffectiveJava3Sandbox/src/item26/Test26.java
+++ EffectiveJava3Sandbox/src/item26/Test26.java
@@ -21,10 +21,17 @@ class Test26 {
 		
 		//Stamp stamp1 = stamps.get(0);//Type missmuch(Canot Compile)
 		Stamp stamp1 = (Stamp) stamps.get(0);
-		Stamp stamp2 = (Stamp) stamps.get(1);//ClassCastException 実行時例外!!!
+		//Stamp stamp2 = (Stamp) stamps.get(1);//ClassCastException 実行時例外!!!
 		
-
-		System.out.println();
+		List<Stamp> stamps2 = new ArrayList<>();
+		stamps2.add(new Stamp("big"));
+		//stamps2.add(new Coin(5));//コンパイル時にエラー発見可能！
+		
+		List rowList = new ArrayList();
+		rowList.add(new ArrayList<String>());
+		
+		List<String> genericList = new ArrayList<String>();
+		//genericList.add(new ArrayList<String>()); エラーになる！知らなかった。
 	}
 
 }
