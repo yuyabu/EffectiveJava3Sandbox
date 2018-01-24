24980fa1bdf2fae971c2efb4b8ea2db21181c7f2
yuyabu <yuyabushita@gmail.com>
2018-01-24 12:28:54 +0900

    add row type sample

--- EffectiveJava3Sandbox/src/item26/Code26.java
+++ EffectiveJava3Sandbox/src/item26/Code26.java
@@ -3,7 +3,15 @@ package item26;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 public class Code26 {
+	static int rowTypeSample(Set s1, Set s2) {
+		int result = 0;
+		for(Object o1: s1)
+			if(s2.contains(o1))
+				result++;
+		return result;
+	}
 	
 }
