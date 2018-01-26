9816f442d92b06412efe3a759705d00d4a1da920
yuyabu <yuyabushita@gmail.com>
2018-01-26 23:28:46 +0900

    原型と非境界ワイルドカードの補足コメントを追記

--- EffectiveJava3Sandbox/src/item26/Code26.java
+++ EffectiveJava3Sandbox/src/item26/Code26.java
@@ -6,6 +6,12 @@ import java.util.List;
 import java.util.Set;
 
 public class Code26 {
+	/**
+	 * raw typeを使った例。禁止すべき。
+	 * @param s1
+	 * @param s2
+	 * @return
+	 */
 	static int rowTypeSample(Set s1, Set s2) {
 		int result = 0;
 		for(Object o1: s1)
@@ -13,6 +19,12 @@ public class Code26 {
 				result++;
 		return result;
 	}
+	/**
+	 * 非境界ワイルドカード(unbounded wild card)を使った例。原型(row type)よりこちらを使うべき。
+	 * @param s1
+	 * @param s2
+	 * @return
+	 */
 	static int unboundedWildcardSample(Set<?> s1, Set<?> s2) {
 		int result = 0;
 		for(Object o1: s1)
