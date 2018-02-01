07c9e4de58a8867a6544495c09b05b88390c56f4
yuyabu <yuyabushita@gmail.com>
2018-02-01 19:23:59 +0900

    generics系の警告がでるコードを追記。

---
+++ EffectiveJava3Sandbox/src/item27/Code27.java
@@ -0,0 +1,16 @@
+package item27;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Code27 {
+	
+	//以下のコードは警告を複数発生させる。
+	Set<String> exaltation = new HashSet();
+
+	//Multiple markers at this line
+	//- Type safety: The expression of type HashSet needs unchecked 
+	// conversion to conform to Set<String>
+	//- HashSet is a raw type. References to generic type HashSet<E> should 
+	// be parameterized
+}
