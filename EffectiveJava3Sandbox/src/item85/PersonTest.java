806d495171998f8c183020533ebc856505bbc12a
yuyabu <yuyabushita@gmail.com>
2018-03-26 00:34:10 +0900

    冗長な{}を削除

--- EffectiveJava3Sandbox/src/item85/PersonTest.java
+++ EffectiveJava3Sandbox/src/item85/PersonTest.java
@@ -13,34 +13,34 @@ import org.junit.jupiter.api.Test;
 class PersonTest {
 
 	@Test
-	//sample from:https://www.ibm.com/developerworks/jp/java/library/j-5things1/index.html
+	// sample
+	// from:https://www.ibm.com/developerworks/jp/java/library/j-5things1/index.html
 	void test() {
-		{
-			try {
-				Person ted = new Person("Ted", "Neward", 39);
-				Person charl = new Person("Charlotte", "Neward", 38);
-				ted.setSpouse(charl);
-				charl.setSpouse(ted);
-				FileOutputStream fos = new FileOutputStream("tempdata.ser");
-				ObjectOutputStream oos = new ObjectOutputStream(fos);
-				oos.writeObject(ted);
-				oos.close();
-			} catch (Exception ex) {
-				fail("Exception thrown during test: " + ex.toString());
-			}
-			try {
-				FileInputStream fis = new FileInputStream("tempdata.ser");
-				ObjectInputStream ois = new ObjectInputStream(fis);
-				Person ted = (Person) ois.readObject();
-				ois.close();
-				assertEquals(ted.getFirstName(), "Ted");
-				assertEquals(ted.getSpouse().getFirstName(), "Charlotte");
-				// Clean up the file
-				new File("tempdata.ser").delete();
-			} catch (Exception ex) {
-				fail("Exception thrown during test: " + ex.toString());
-			}
+		try {
+			Person ted = new Person("Ted", "Neward", 39);
+			Person charl = new Person("Charlotte", "Neward", 38);
+			ted.setSpouse(charl);
+			charl.setSpouse(ted);
+			FileOutputStream fos = new FileOutputStream("tempdata.ser");
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(ted);
+			oos.close();
+		} catch (Exception ex) {
+			fail("Exception thrown during test: " + ex.toString());
 		}
+		try {
+			FileInputStream fis = new FileInputStream("tempdata.ser");
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			Person ted = (Person) ois.readObject();
+			ois.close();
+			assertEquals(ted.getFirstName(), "Ted");
+			assertEquals(ted.getSpouse().getFirstName(), "Charlotte");
+			// Clean up the file
+			new File("tempdata.ser").delete();
+		} catch (Exception ex) {
+			fail("Exception thrown during test: " + ex.toString());
+		}
+
 	}
 
 }
