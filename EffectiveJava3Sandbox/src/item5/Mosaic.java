ed3c9267a77e5d749ec1accf666f502b94d26cb5
yuyabu <yuyabushita@gmail.com>
2018-03-27 00:11:57 +0900

    DIのサンプルをちょっと改変

--- EffectiveJava3Sandbox/src/item5/Mosaic.java
+++ EffectiveJava3Sandbox/src/item5/Mosaic.java
@@ -3,7 +3,18 @@ package item5;
 import java.util.function.Supplier;
 
 public class Mosaic {
-	public static void create(Supplier<? extends Tile> tileFactory) {
-		
+	
+	Mosaic create(Supplier<? extends Tile> tileFactory) {
+		return new Mosaic(tileFactory.get());
+	}
+	Mosaic(){}
+	Mosaic(Tile tile){	
+		this.tile = tile;
+	}
+	private Tile tile;
+	
+	public String asText() {
+		return null;
+		//return "this is "+tile.+""
 	}
 }
--- EffectiveJava3Sandbox/src/item5/Tile.java
+++ EffectiveJava3Sandbox/src/item5/Tile.java
@@ -1,5 +1,10 @@
 package item5;
 
 public class Tile {
+	private String color;
+	Tile(String color){
+		this.color = color;
+	}
+	
 
 }
