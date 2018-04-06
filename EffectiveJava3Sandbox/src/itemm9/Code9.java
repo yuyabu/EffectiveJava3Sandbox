dc52210c25417ae614057ff6522525e282f3403a
yuyabu <yuyabushita@gmail.com>
2018-04-07 03:37:01 +0900

    add wrong implementation

--- EffectiveJava3Sandbox/src/itemm9/Code9.java
+++ EffectiveJava3Sandbox/src/itemm9/Code9.java
@@ -1,10 +1,15 @@
 package itemm9;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class Code9 {
+	static final int BUFFER_SIZE = 1024;
 	static String firstLineOfFile(String path)throws IOException{
 		BufferedReader br = new BufferedReader(new FileReader(path));
 		try {
@@ -12,6 +17,22 @@ public class Code9 {
 			
 		}finally {
 			br.close();
+		}
+	}
+	static void copy(String src,String dst)throws IOException{
+		InputStream in = new FileInputStream(src);
+		try {
+			OutputStream out = new FileOutputStream(dst);
+			try {
+				byte[] buf = new byte[BUFFER_SIZE];
+				int n;
+				while ((n = in.read(buf)) >=0)
+					out.write(buf,0,n);
+			}finally {
+				out.close();
+			}
+		}finally {
+			in.close();
 		}
 	}
 }
