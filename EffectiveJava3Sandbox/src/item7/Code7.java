3777523f9647e3e15b8ddcb42104b61885295389
yuyabu <yuyabushita@gmail.com>
2018-04-06 17:42:07 +0900

    例えが間違っていたので訂正

--- EffectiveJava3Sandbox/src/item7/Code7.java
+++ EffectiveJava3Sandbox/src/item7/Code7.java
@@ -5,12 +5,22 @@ import java.util.Map;
 import java.util.WeakHashMap;
 
 public class Code7 {
-	Map<Integer, String> studentList;
-	Map<Integer, String> blackList;
-	public Code7(Map<Integer, String> studentList, Map<Integer, String> blackList) {
+	Map<String, String> studentList;
+	Map<String, String> blackList;
+	public Code7(Map<String, String> studentList, Map<String, String> blackList) {
 		super();
 		this.studentList = studentList;
 		this.blackList = blackList;
+	}
+	public void registBlackList(String key) {
+		if (studentList.containsKey(key)){
+			blackList.put(key,studentList.get(key)); 
+		}
+	}
+	public String toString() {
+		return new StringBuilder()
+				.append("studentList=").append(studentList.toString())
+				.append("blackList=").append(blackList.toString()).toString();
 	}
 	
 }
--- EffectiveJava3Sandbox/src/item7/Test7.java
+++ EffectiveJava3Sandbox/src/item7/Test7.java
@@ -3,6 +3,7 @@ package item7;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.WeakHashMap;
 
 import org.junit.jupiter.api.Test;
@@ -11,17 +12,35 @@ class Test7 {
 
 	@Test
 	void test() {
+		
 		Code7 instance = new Code7(new WeakHashMap<>(),new HashMap<>());
-		instance.studentList.put(1,"田中太郎");
-		instance.studentList.put(2,"山田太郎");
-		instance.studentList.put(3,"佐藤太郎");
+		String studentNo1 = new String("1");
+		String studentNo2 = new String("2");
+		String studentNo3 = new String("3");
 		
-		System.out.println(instance.studentList);
+		instance.studentList.put(studentNo1,new String("田中太郎"));
+		instance.studentList.put(studentNo2,new String("山田太郎"));
+		instance.studentList.put(studentNo3,new String("佐藤太郎"));
 		
+		System.out.println(instance);
+		
+		instance.registBlackList(studentNo3);
+		System.out.println(instance);
+
+		//強参照を全て消す
+		studentNo1 = null;
+		studentNo2 = null;
+		studentNo3 = null;
+		
+		System.gc();
+		
+		System.out.println(instance);
+		System.out.println(instance.studentList.get("1"));//null
 		
 		
 		
 	}
+	
 
 
 }
